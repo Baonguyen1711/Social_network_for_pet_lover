a4457cb69ed3f6b4359a4008b52a691e840b117f
Baonguyen1711 <128447276+Baonguyen1711@users.noreply.github.com>
2025-01-04 16:59:58 +0700

    Update register.js

--- backend/src/middlewares/auth/register.js
+++ backend/src/middlewares/auth/register.js
@@ -1,4 +1,4 @@
-const User = require('../../models/user')
+const User = require('../../models/User')
 const connectToDb = require('../../config/database/db')
 
 const validate = (req, res, next) => {
